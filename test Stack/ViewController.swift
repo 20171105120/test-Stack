49286f77d23e89546ae631c730d91f4841f05d0f
20171105120 <476625879@qq.com>
2018-11-15 09:09:50 +0800

    Stack 代码

--- test Stack/ViewController.swift
+++ test Stack/ViewController.swift
@@ -8,6 +8,31 @@
 
 import UIKit
 
+public struct Stack<T>
+{
+    fileprivate var array = [T]()
+    public var isEmpty: Bool
+    {
+        return array.isEmpty
+    }
+    public var count: Int
+    {
+        return array.count
+    }
+    public mutating func push(_ element: T)
+    {
+        array.append(element)
+    }
+    public mutating func pop() -> T?
+    {
+        return array.popLast()
+    }
+    public var peak: T?
+    {
+        return array.last
+    }
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
